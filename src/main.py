13ea09d5894d635231aa2d068d02991e08d6b04f
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-18 18:24:27 +0800

    feat: added argparse

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-from spotify import get_token, get_playlist_tracks
+from spotify import get_token, get_playlist_tracks, display_playlist_tracks
 from youtube import get_youtube_url
 from downloader import download
+import argparse
+
 
 from dotenv import load_dotenv
 import os
+import sys
 
 if __name__ == '__main__':
     load_dotenv()
@@ -12,13 +15,33 @@ if __name__ == '__main__':
     client_secret = os.getenv("CLIENT_SECRET")
     token = get_token(client_id, client_secret)
 
-    playlist_id = input('Enter playlist id: ')
-    tracks = get_playlist_tracks(token, playlist_id)
-    path = input('Enter download path: ')
+    parser = argparse.ArgumentParser(description='A Spotify playlist downloader.')
+    
+    parser.add_argument('-ls', action='store_true', help='List the tracks in the playlist.')
+    parser.add_argument('-d', action='store_true', help='Download the given playlist.')
 
-    for track in tracks:
-        print(f'Downloading: {track}')
-        url = get_youtube_url(track)
-        download(track, url, path)
+    parser.add_argument('--playlist', help='Playlist to download', required=True)
+    parser.add_argument('--path', help='Downlaod path', default='.')
 
-    print('Download finished!')
+    args = parser.parse_args()
+
+    playlist = args.playlist
+    path = args.path
+
+    tracks = get_playlist_tracks(token, playlist)
+
+    if args.ls:
+        display_playlist_tracks(tracks)
+        exit()
+
+    if args.d:
+        print('Downloading playlist...')
+        for track in tracks:
+            print(f'Downloading: {track}')
+            url = get_youtube_url(track)
+            download(track, url, path)
+
+            print('Download finished!')
+    
+    if not args.ls and not args.d:
+            sys.exit('Usage: python3 main.py [-ls] [-d] <playlist_id> <path>')
