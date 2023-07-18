3c7802d504f07e54176c97f5c6ace2af09077721
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-18 16:43:37 +0800

    feat: added html parser

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,9 @@
+from bs4 import BeautifulSoup
+
 def get_auth_header(token):
     return {
         'Authorization': f'Bearer {token}'
     }
+
+def html_page(page):
+    return BeautifulSoup(page.text, 'html.parser')
