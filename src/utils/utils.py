f1191b114f7ae9cd8ee73c5fc3a780ea65222550
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-18 18:09:54 +0800

    refactor: remove unncessary imports

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,4 @@
-from bs4 import BeautifulSoup
-
 def get_auth_header(token):
     return {
         'Authorization': f'Bearer {token}'
     }
-
-def html_page(page):
-    return BeautifulSoup(page.text, 'html.parser')
