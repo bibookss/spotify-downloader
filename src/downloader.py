d7adb83e67570b4243e6b6dc1445715302ab2a10
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-07-18 17:58:01 +0800

    feat: download to m4a

--- src/downloader.py
+++ src/downloader.py
@@ -2,9 +2,16 @@ import yt_dlp
 import os
 
 def download(title, link, path):
-  with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
-    info_dict = video.extract_info(link, download = True)
-    video_title = info_dict['title']
-    print(video_title)
-    video.download(link)    
-    os.rename(f'{video_title}.mp3', f'{path}/{title}.mp3')
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'postprocessors': [{  
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+        }],
+        'outtmpl': f'{path}/{title}.%(ext)s'
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+    
